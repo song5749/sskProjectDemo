24f12260c9705911e3d1e0a668d6e0908243907c
song5749 <625535361@qq.com>
2018-12-08 14:56:50 +0800

    init

---
+++ ssk-spring-boot-test/src/main/java/com/ssk/sskspringboottest/SskSpringBootTestApplication.java
@@ -0,0 +1,12 @@
+package com.ssk.sskspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SskSpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SskSpringBootTestApplication.class, args);
+    }
+}
